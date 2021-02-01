9e7b519631c84d08f24b4bcf129ebdc9010b8856
Jimmy Z. Lin <43326009+forestphilosophy@users.noreply.github.com>
2021-02-01 17:48:26 +0100

    Update Day 4: Binomial Distribution II.py

--- 10 Days of Statistics/Day 4: Binomial Distribution II.py
+++ 10 Days of Statistics/Day 4: Binomial Distribution II.py
@@ -5,10 +5,10 @@ def binom(n, k):
 p,n = map(int,input().split())
 p = p/100
 ans = []
-fianl_list = [[i for i in range(3)]]
+final_list = [[i for i in range(3)]]
 list_2 = [i for i in range(2,n+1)]
-fianl_list.append(list_2)
+final_list.append(list_2)
 
 for i in range(2):
-    pmf = [binom(n,k) * math.pow(p,k) * math.pow(1-p,n-k) for k in fianl_list[i]]
+    pmf = [binom(n,k) * math.pow(p,k) * math.pow(1-p,n-k) for k in final_list[i]]
     print (round(sum(pmf),3))
